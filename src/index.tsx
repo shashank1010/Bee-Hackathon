871c9efdeec3449ce8d36c40251fd833b33cb928
Shashank Agarwal <shashank1010@gmail.com>
2022-07-16 14:44:46 +0300

    React and Redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StoreProvider } from './components/StoreProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <StoreProvider>
+        <App />
+      </StoreProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
